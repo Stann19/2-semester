e08468876915191070eeb802b8627cb0471db14b
Stann19 <pstanislav2003@gmail.com>
2022-04-02 15:57:42 +0300

    homework

---
+++ 29.03.2022/29.03.2022(1).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct student {
+    string name;
+    int group;
+    int ses[5];
+};
+
+void checking(student stud1[]) {
+    int l = 0;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (stud1[i].ses[j] == 2) {
+                cout << stud1[i].name << " " << stud1[i].group << '\n';
+                l++;
+                break;
+            }
+        }
+    }
+    if (l == 0) cout << "There are no students with a mark of 2";
+}
+
+int main()
+{
+    student stud1[2];
+    for (int i = 0; i < 2; i++) {
+            cout << "name:";
+            cin >> stud1[i].name;
+            cout << "group:";
+            cin >> stud1[i].group;
+            cout << "ses:";
+            for (int j = 0; j < 5; j++) {
+                cin >> stud1[i].ses[j];
+            }
+    }
+    checking(stud1);
+    return 0;
+}
+
---
+++ 29.03.2022/29.03.2022(2).cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct student {
+    string name;
+    string surname;
+    int age;
+    int classh;
+    int number;
+    int marks[30]{0};
+};
+
+void add_mark(student* a, int mark) {
+    (* a).number += 1;
+    (* a).marks[(* a).number - 1] = mark;
+}
+
+void input(student* a, float s[], int i) {
+    cout << "name:";
+    cin >> (*a).name >> (*a).surname;
+    cout << "age:";
+    cin >> (*a).age;
+    cout << "class:";
+    cin >> (*a).classh;
+    cout << "number of marks:";
+    cin >> (*a).number;
+    cout << "marks:";
+    for (int j = 0; j < (*a).number; j++) {
+        cin >> (*a).marks[j];
+        s[i] += (*a).marks[j];
+    }
+}
+
+void output(student* a, float s[], int i) {
+    cout << "name: " << (*a).name << " " << (*a).surname << '\n';
+    cout << "age: " << (*a).age << '\n';
+    cout << "class: " << (*a).classh << '\n';
+    cout << "number of marks: " << (*a).number << '\n';
+    cout << "marks: ";
+    for (int j = 0; j < (*a).number; j++) {
+        cout << (*a).marks[j] << " ";
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    student stud1[5];
+    float s[5]{ 0.f };
+    for (int i = 0; i < 5; i++) {
+        input(&stud1[i], s, i);
+    }
+    for (int i = 0; i < 5; i++) {
+        while (s[i] / stud1[i].number < 4.5) {
+            add_mark(&stud1[i], 5);
+            s[i] += stud1[i].marks[stud1[i].number - 1];
+        }
+    }
+    for (int i = 0; i < 5; i++) {
+        output(&stud1[i], s, i);
+    }
+    return 0;
+}
