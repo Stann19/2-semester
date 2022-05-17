a456068d37e19e356b24ad3c8069f1daeb619ac4
Stann19 <pstanislav2003@gmail.com>
2022-05-17 07:50:11 +0300

    homework

---
+++ 26.04.2022/26.04.2022(1).cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+struct BinaryTree {
+    int data;
+    BinaryTree* left;
+    BinaryTree* right;
+};
+
+void osnov(BinaryTree* &root, int a) {
+    if (root == nullptr) {
+        root = new BinaryTree;
+        root->data = a;
+        root->left = nullptr;
+        root->right = nullptr;
+        return;
+    }
+    if (a > root->data) {
+        osnov(root->right, a);
+    }
+    else {
+        osnov(root->left, a);
+    }
+}
+
+void del(BinaryTree* &root)
+{
+    if (root != nullptr)
+    {
+        del(root->left);
+        del(root->right);
+        delete root;
+    }
+}
+
+void output(BinaryTree* &root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    else
+    {
+        cout << "(";
+        output(root->left);
+        cout << root->data;
+        output(root->right);
+        cout << ")";
+    }
+}
+
+int main()
+{
+    int a;
+    cin >> a;
+    BinaryTree* root = NULL;
+    while (a != 0)
+    {
+        osnov(root, a);
+        cin >> a;
+    }
+    output(root);
+    del(root);
+}
+
