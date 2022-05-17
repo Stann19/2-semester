d77c5c9795b7ab1e9039289ed040eaa41dfea667
Stann19 <pstanislav2003@gmail.com>
2022-05-17 04:37:14 +0300

    homework

---
+++ 19.04.2022/19.04.2022(1).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+void input(int a[], int n) {
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+}
+
+int osnov(int a[], int n, int mem[]) {
+	if (n == 1) {
+		mem[n] = 1;
+		return mem[n];
+	}
+	if (n > 1 && a[n - 1] > a[n - 2]) {
+		mem[n - 1] = osnov(a, n - 1, mem) + 1;
+		return mem[n - 1];
+	}
+	else mem[n - 1] = osnov(a, n - 1, mem);
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int* a = new int[n] {0};
+	input(a, n);
+	int* mem = new int[n]{ 0 };
+	cout << osnov(a, n, mem);
+	delete[] a;
+	delete[] mem;
+}
---
+++ 19.04.2022/19.04.2022(2).cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+void input(int a[], int n) {
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+}
+
+int min(int a, int b) {
+    if (b > a) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+int osnov(int a[], int n) {
+    int E;
+    if (n == 1) return E = 0;
+    else if (n == 2) return abs(a[n - 1] - a[n - 2]);
+    return E = min(osnov(a, n - 1) + abs(a[n - 1] - a[n - 2]), osnov(a, n - 2) + 3 * abs(a[n - 1] - a[n - 3]));
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int* a = new int[n] {0};
+    input(a, n);
+    cout << osnov(a, n);
+}
